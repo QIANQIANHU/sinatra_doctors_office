a5a05e39c78c1e7a32bc594965bd6882e3513d21
Malgorzata Haniszewska and Qianqian Hu <malgorzata@>
2017-12-06 09:07:03 -0800

    add app.rb to swich between views

---
+++ app.rb
@@ -0,0 +1,52 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/doctor')
+require('./lib/patient')
+also_reload('lib/**/*.rb')
+require("pg")
+
+DB = PG.connect({:dbname => "doctors_office"})
+
+
+get("/") do
+  erb(:index)
+end
+
+get("/admin") do
+  erb(:admin)
+end
+
+get("/doctors_list") do
+  @doctors = Doctor.all()
+  erb(:doctors_list)
+end
+
+get("/form_doctor") do
+  erb(:form_doctor)
+end
+
+get("/form_patient") do
+  erb(:form_patient)
+end
+
+post("/doctors") do
+  name = params.fetch("name")
+  speciality = params.fetch("speciality")
+  doctor = Doctor.new({:name => name,:speciality => speciality, :id => nil})
+  doctor.save()
+  erb(:admin)
+ end
+
+ post("/patients") do
+   name = params.fetch("name")
+   birthdate = params.fetch("birthdate")
+   doctor_id = params.fetch("doctor_id")
+   patient = Patient.new({:name => name,:birthdate => birthdate, :doctor_id => doctor_id})
+   patient.save()
+   erb(:admin)
+  end
+
+get("/doctor/:id") do
+  @doctor = Doctor.find(params.fetch("id").to_i())
+  erb(:doctor)
+end
